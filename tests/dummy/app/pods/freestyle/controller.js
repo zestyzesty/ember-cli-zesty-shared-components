29b81ac4e6c074eb59450317fc172408185f59cf
Ben Holmes <mail@benholmes.uk>
2016-12-20 18:23:33 +0000

    Add style guide usage instruction

--- tests/dummy/app/pods/freestyle/controller.js
+++ tests/dummy/app/pods/freestyle/controller.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 import FreestyleController from 'ember-freestyle/controllers/freestyle';
+import moment from 'moment';
+import { task } from 'ember-concurrency';
 
 const { inject } = Ember;
 
 export default FreestyleController.extend({
   emberFreestyle: inject.service(),
+
+  date: moment("2016-10-30T10:30"),
+  interprettedDate: moment("2016-10-30T10:30"),
+  updateDate: task(function*(date) {
+    yield this.set('interprettedDate', date);
+  }),
 
   statusFilterEntries: [
     Ember.Object.create({ status: 'new' }),
